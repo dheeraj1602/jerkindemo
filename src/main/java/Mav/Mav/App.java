56b4d22d87e409e1f1285b0a9d4cd3573f622670
unknown <dheerajreddy1602@gmail.com>
2023-08-17 16:59:44 +0530

    first

---
+++ src/main/java/Mav/Mav/App.java
@@ -0,0 +1,13 @@
+package Mav.Mav;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
